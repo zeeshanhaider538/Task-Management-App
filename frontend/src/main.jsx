ddd87a5c170b2fbb9b71e396ae715eaae8635df7
Zeeshan Haider <zeeshanhaider538@gmail.com>
2025-10-17 01:07:12 +0500

    update the main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "antd/dist/reset.css"; // AntD CSS reset
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+// src/main.jsx
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import "antd/dist/reset.css"; // AntD CSS reset
+// import "./index.css";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
