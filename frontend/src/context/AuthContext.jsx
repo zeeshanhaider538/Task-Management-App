b006c0394c5da84803b2049371de75f9e73badb9
Zeeshan Haider <zeeshanhaider538@gmail.com>
2025-10-17 01:05:51 +0500

    added auth component

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,29 @@
+// src/context/AuthContext.jsx
+import { createContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(() =>
+    JSON.parse(localStorage.getItem("user"))
+  );
+  const navigate = useNavigate();
+
+  const login = (data) => {
+    localStorage.setItem("user", JSON.stringify(data));
+    setUser(data);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+    navigate("/login");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
